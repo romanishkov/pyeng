b27fa67a88e227ba71d8cebdc047576032a83632
romanishkov <ishkov.roma@gmail.com>
2023-04-17 18:16:59 +0300

    17_2-3 done

--- exercises/17_serialization/answer_task_17_2.py
+++ exercises/17_serialization/answer_task_17_2.py
@@ -42,30 +42,30 @@
 
 Кроме того, создан список заголовков (headers), который должен быть записан в CSV.
 """
-
-import glob
-import csv
 import re
+import csv
+import glob
 
 
-def parse_sh_version(sh_ver_str):
+def parse_sh_version(sh_ver_output):
     regex = (
         "Cisco IOS .*? Version (?P<ios>\S+), .*"
         "uptime is (?P<uptime>[\w, ]+)\n.*"
         'image file is "(?P<image>\S+)".*'
     )
-    match = re.search(regex, sh_ver_str, re.DOTALL)
+    match = re.search(regex, sh_ver_output, re.DOTALL,)
     if match:
         return match.group("ios", "image", "uptime")
 
 
 def write_inventory_to_csv(data_filenames, csv_filename):
     headers = ["hostname", "ios", "image", "uptime"]
-    with open(csv_filename, "w", newline='') as dest:
-        writer = csv.writer(dest, quoting=csv.QUOTE_NONNUMERIC)
+    with open(csv_filename, "w") as f:
+        writer = csv.writer(f)
         writer.writerow(headers)
+
         for filename in data_filenames:
-            hostname = re.search(r"sh_version_(.+)\.txt", filename).group(1)
+            hostname = re.search("sh_version_(\S+).txt", filename).group(1)
             with open(filename) as f:
                 parsed_data = parse_sh_version(f.read())
                 if parsed_data:
@@ -74,4 +74,4 @@ def write_inventory_to_csv(data_filenames, csv_filename):
 
 if __name__ == "__main__":
     sh_version_files = glob.glob("sh_vers*")
-    write_inventory_to_csv(sh_version_files, 'routers_inventory.csv')
+    write_inventory_to_csv(sh_version_files, "routers_inventory.csv")
--- exercises/17_serialization/answer_task_17_3.py
+++ exercises/17_serialization/answer_task_17_3.py
@@ -27,17 +27,20 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 import re
 
 
-def parse_sh_cdp_neighbors(input_str):
-    regex = (r"(?P<rem_host>\S+)\s+(?P<local_intf>\S+ \d+/\d+).*\s+(?P<remote_intf>\S+ \d+/\d+)")
-    loc_host = re.search(r"^\n*(\S+)[>\#]", input_str).group(1)
-    out_dict = {loc_host: {}}
-    for match in re.finditer(regex, input_str):
-        rem_host, loc_int, rem_int = match.group('rem_host', 'local_intf', 'remote_intf')
-        out_dict[loc_host][loc_int] = {rem_host: rem_int}
-    return out_dict
+def parse_sh_cdp_neighbors(command_output):
+    regex = re.compile(
+        r"(?P<r_dev>\w+)  +(?P<l_intf>\S+ \S+)"
+        r"  +\d+  +[\w ]+  +\S+ +(?P<r_intf>\S+ \S+)"
+    )
+    connect_dict = {}
+    l_dev = re.search(r"(\S+)[>#]", command_output).group(1)
+    connect_dict[l_dev] = {}
+    for match in regex.finditer(command_output):
+        r_dev, l_intf, r_intf = match.group("r_dev", "l_intf", "r_intf")
+        connect_dict[l_dev][l_intf] = {r_dev: r_intf}
+    return connect_dict
 
 
 if __name__ == "__main__":
     with open("sh_cdp_n_sw1.txt") as f:
         print(parse_sh_cdp_neighbors(f.read()))
-
